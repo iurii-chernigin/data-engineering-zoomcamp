7587d8ccf26a3dfca163aea9495be78b876dcc68
Iurii Chernigin <chernigin.y@gmail.com>
2023-02-07 17:00:07 +0000

    week2-prefect (BigQuery ETL): implemet data loading

--- week2/prefect-flows/etl_gcs_to_bq.py
+++ week2/prefect-flows/etl_gcs_to_bq.py
@@ -15,6 +15,7 @@ def extract_from_gcs(gcs_block_name: str, color: str, year: int, month: int) -> 
     gcs_block.get_directory(from_path=gcs_path, local_path="./")
     return gcs_path
 
+
 @task(
     retries=3, 
     retry_delay_seconds=[10, 30, 60],
@@ -25,12 +26,25 @@ def transform(path: str) -> pd.DataFrame:
     
     na_passangers_count = df["passenger_count"].isna().sum()
     print(f"Pre-transform: rows with null value in passenger_count field: {na_passangers_count}")
-    
     df["passenger_count"].fillna(0, inplace=True)
     na_passangers_count = df["passenger_count"].isna().sum()
     print(f"Post-transform: rows with null value in passenger_count field: {na_passangers_count}")
 
     return df
+
+
+@task
+def write_bq(gcp_creds_blockname: str, gcp_project_id: str, trips_df: pd.DataFrame, bq_table: str) -> None:
+    """Write dataframe to BigQuery"""
+    gcp_creds_block = GcpCredentials.load(gcp_creds_block)
+    pd.to_gbq(
+        destination_table=bq_table,
+        project_id=gcp_project_id,
+        credentials=gcp_cresd_block.get_credentials_from_service_account(),
+        chunk_size=500_000,
+        if_exists="append"
+    )
+    
 
 @flow(
     name="Uploading trip data to BigQuery",
@@ -38,14 +52,20 @@ def transform(path: str) -> pd.DataFrame:
     version=os.getenv("GIT_COMMIT_SHA"))
 def etl_gcs_to_bq():
     """Main ETL flow to load data to BigQuery"""
-    color = "yellow"
-    year = 2021
-    month = 1
+    # Sorce dataset parameters
+    dataset_color = "yellow"
+    dataset_year = 2021
+    dataset_month = 1
+    # Destination parameters of dataset
+    gcs_bucket_block_name = "gcs-bucket-prefect-flows"
+    gcp_creds_block_name = "gcp-creds-prefect-flows"
+    gcp_project_id = "secure-analyzer-375018"
+    bq_table = "zoomcamp.yellow_taxi_rides"
 
-    gcs_block_name = "gcs-bucket-prefect-flows"
-
-    path = extract_from_gcs(gcs_block_name, color, year, month)
-    df = transform(path)
+    # Task launch
+    dataset_local_path = extract_from_gcs(gcs_bucket_block_name, dataset_color, dataset_year, dataset_month)
+    trips_df = transform(dataset_local_path)
+    write_bq(gcp_creds_block_name, trips_df, bq_table)
 
 
 if __name__ == "__main__":
