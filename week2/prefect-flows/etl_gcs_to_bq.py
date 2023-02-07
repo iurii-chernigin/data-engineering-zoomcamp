d91b184a4cc4275df1b88b10d44b7514ddf9556f
Iurii Chernigin <chernigin.y@gmail.com>
2023-02-07 18:21:52 +0000

    week2-prefect (BigQuery ETL): create parametrizing flow

--- week2/prefect-flows/etl_gcs_to_bq.py
+++ week2/prefect-flows/etl_gcs_to_bq.py
@@ -23,25 +23,25 @@ def extract_from_gcs(gcs_block_name: str, color: str, year: int, month: int) -> 
 def transform(path: str) -> pd.DataFrame:
     """Data Cleaning"""
     df = pd.read_parquet(path)
-    
     na_passangers_count = df["passenger_count"].isna().sum()
     print(f"Pre-transform: rows with null value in passenger_count field: {na_passangers_count}")
     df["passenger_count"].fillna(0, inplace=True)
     na_passangers_count = df["passenger_count"].isna().sum()
     print(f"Post-transform: rows with null value in passenger_count field: {na_passangers_count}")
-
     return df
 
 
-@task
-def write_bq(gcp_creds_blockname: str, gcp_project_id: str, trips_df: pd.DataFrame, bq_table: str) -> None:
+@task(
+    retries=3,
+    etry_delay_seconds=[10, 30, 60])
+def write_bq(gcp_creds_block_name: str, gcp_project_id: str, trips_df: pd.DataFrame, bq_table: str) -> None:
     """Write dataframe to BigQuery"""
-    gcp_creds_block = GcpCredentials.load(gcp_creds_block)
-    pd.to_gbq(
+    gcp_creds_block = GcpCredentials.load(gcp_creds_block_name)
+    trips_df.to_gbq(
         destination_table=bq_table,
         project_id=gcp_project_id,
-        credentials=gcp_cresd_block.get_credentials_from_service_account(),
-        chunk_size=500_000,
+        credentials=gcp_creds_block.get_credentials_from_service_account(),
+        chunksize=500_000,
         if_exists="append"
     )
     
@@ -49,23 +49,45 @@ def write_bq(gcp_creds_blockname: str, gcp_project_id: str, trips_df: pd.DataFra
 @flow(
     name="Uploading trip data to BigQuery",
     description="Get Green Taxi parquet from GCS bucket, clean, and load to BigQuery",
-    version=os.getenv("GIT_COMMIT_SHA"))
-def etl_gcs_to_bq():
+    version=os.getenv("GIT_COMMIT_SHA")
+)
+def etl_gcs_to_bq(
+    dataset_color: str,
+    dataset_year: int,
+    dataset_months: list[int],
+    gcs_bucket_block_name: str,
+    gcp_creds_block_name: str,
+    gcp_project_id: str,
+    bq_table: str
+):
     """Main ETL flow to load data to BigQuery"""
-    # Sorce dataset parameters
-    dataset_color = "yellow"
-    dataset_year = 2021
-    dataset_month = 1
-    # Destination parameters of dataset
-    gcs_bucket_block_name = "gcs-bucket-prefect-flows"
-    gcp_creds_block_name = "gcp-creds-prefect-flows"
-    gcp_project_id = "secure-analyzer-375018"
-    bq_table = "zoomcamp.yellow_taxi_rides"
-
     # Task launch
     dataset_local_path = extract_from_gcs(gcs_bucket_block_name, dataset_color, dataset_year, dataset_month)
     trips_df = transform(dataset_local_path)
-    write_bq(gcp_creds_block_name, trips_df, bq_table)
+    write_bq(gcp_creds_block_name, gcp_project_id, trips_df, bq_table)
+
+
+@flow
+def etl_gcs_to_bq_base(
+    dataset_color: str = "yellow",
+    dataset_year: int = 2021,
+    dataset_months: list[int] = [1],
+    gcs_bucket_block_name: str = "gcs-bucket-prefect-flows",
+    gcp_creds_block_name: str = "gcp-creds-prefect-flows",
+    gcp_project_id: str = "secure-analyzer-375018",
+    bq_table: str = "zoomcamp.yellow_taxi_rides"
+):
+    """Parent ETL flow for parametrizing runs"""
+    for dataset_month in dataset_months:
+        etl_gcs_to_bq(
+            dataset_color,
+            dataset_year,
+            dataset_months,
+            gcs_bucket_block_name,
+            gcp_creds_block_name,
+            gcp_project_id,
+            bq_table
+        )
 
 
 if __name__ == "__main__":
