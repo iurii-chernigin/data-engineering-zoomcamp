fc30e506b0d733ed1b8f9f118fe3d53387466cb8
Iurii Chernigin <chernigin.y@gmail.com>
2023-02-09 14:51:41 +0000

    week2-prefect-gcs-flow: fix flow args issue and change default t value of args

--- week2/prefect-flows/etl_web_to_gcs.py
+++ week2/prefect-flows/etl_web_to_gcs.py
@@ -17,8 +17,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -60,17 +60,17 @@ def etl_web_to_gcs(
     return df_clean.shape[0]
 
 
-@flow()
+@flow(log_prints=True)
 def etl_web_to_gcs_base(
-    color: str = "yellow",
-    year: int = 2019,
+    color: str = "green",
+    year: int = 2020,
     months: list[int] = [1],
-    local_data_path: str = "data"
+    local_data_path: str = "data",
     gcs_data_path: str = "data"
 ):
-    rows_precessed = 0
+    rows_processed = 0
     for month in months:
-        rows_precessed += etl_web_to_gcs(color, year, month)
+        rows_processed += etl_web_to_gcs(color, year, month, local_data_path, gcs_data_path)
     print(f"Flow cycle is finished, rows processed: {rows_processed}")
 
 
